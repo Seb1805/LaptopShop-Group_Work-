94bc66eb422ef93190d55a19b924875c3d2395cd
Christian Dahl Jeppesen <cdj@Unit-it.dk>
2021-06-09 13:01:28 +0200

    Address

---
+++ LaptopShop/Model/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaptopShop.Model
+{
+    class Address
+    {
+        public int AddressId { get; set; }
+        public int CustomerId { get; set; }
+        public string Street { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
