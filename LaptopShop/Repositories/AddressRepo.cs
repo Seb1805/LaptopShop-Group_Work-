1751188c7398f3ff8003557888eb9bea215a396d
Frank <frankjensen156@gmail.com>
2021-06-11 08:19:36 +0200

    interfaces & REPO

---
+++ LaptopShop/Repositories/AddressRepo.cs
@@ -0,0 +1,23 @@
+﻿using LaptopShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LaptopShop.Repositories
+{
+    class AddressRepo
+    {
+        private readonly LaptopContext _laptopContext;
+
+        public AddressRepo(LaptopContext laptopContext)
+        {
+            _laptopContext = laptopContext;
+        }
+
+        public Address GetAddressByID(int id)
+        {
+            return _laptopContext.addresses.First(a => a.AddressId == id);
+        }
+    }
+}
